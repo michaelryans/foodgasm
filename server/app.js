a34567b1147f9e34afb8dc74fd7e075aafcdbea9
Michael <msaputra93@yahoo.com>
2019-05-10 12:55:27 +0700

    dad morgan, error handler

--- server/app.js
+++ server/app.js
@@ -8,12 +8,16 @@ const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/foodgasm', {useNewUrlParser:true})
 const errorHandler = require('./middlewares/errorHandling')
 
+const morgan = require('morgan');
+
 //body parser
 app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({extended: false}))
+app.use(morgan())
 
 app.use('/', router)
+app.use(errorHandler)
 
 app.listen(PORT, () => {
     console.log('app is listening on port ,', PORT)
