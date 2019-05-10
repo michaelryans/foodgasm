4f40a9bdccba21e36d510f2a7564a83b952847e7
Michael <msaputra93@yahoo.com>
2019-05-10 09:48:44 +0700

    add validate email and password - user

--- server/models/user.js
+++ server/models/user.js
@@ -4,9 +4,25 @@ const Schema = mongoose.Schema;
 const userSchema = new Schema({
     email: {
         type: String,
+        validate: [{
+            validator(email) {
+                return User.findOne({email})
+                .then(found => {
+                    if (found) return false;
+                })
+            },
+            message: 'please use valid email address'
+        }, {
+            validator(email) {
+                const regex = /\S+@\S+\.\S+/
+                return regex.test(email)
+            },
+            message: ' please enter valid email adress'
+        }]
     },
     password: {
-        type: String
+        type: String,
+        required: [true, 'password must not be empty']
     }
 });
 
