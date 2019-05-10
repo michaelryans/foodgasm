f08b32b382741b35966f3ecee16cdef7b7c61d05
komangmahendra <komangwp.mahendra@gmail.com>
2019-05-10 09:16:33 +0700

    add route

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,7 +1,10 @@
 const router = require('express').Router()
 const userRouter = require('./user')
 const foodRouter = require('./food')
+const oauthRouter = require('./oauth')
 
 router.use('/foods', foodRouter)
+router.use('/user', userRouter)
+router.use('/oauth', oauthRouter)
 
 module.exports = router
